4d4ba55b35eba7cfa6a148bc5498e2d990dec431
chobeat <simone.robutti@gmail.com>
2017-10-02 10:06:38 +0200

    Removed equals

--- src/main/scala/org/chobeat/scalageohash/GeoPoint.scala
+++ src/main/scala/org/chobeat/scalageohash/GeoPoint.scala
@@ -1,10 +1,4 @@
 package org.chobeat.scalageohash
 
-case class GeoPoint(lat: Double, lon: Double) {
-  override def equals(that: Any): Boolean = that match {
-    case that: GeoPoint => lat.equals(that.lat) && lon.equals(that.lon)
-    case _              => false
-  }
-
-}
+case class GeoPoint(lat: Double, lon: Double)
 
